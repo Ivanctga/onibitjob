6a641f7543bb1bddc072df0a0f1a029353f8ba96
Ivanctga <ivanctga@gmail.com>
2025-04-24 08:56:53 -0300

    Primeiro Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name description])
+    devise_parameter_sanitizer.permit(:sign_up, keys:
+     %i[name description])
   end
 end
